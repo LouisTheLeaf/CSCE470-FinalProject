cb7a6c17d4edeb4c79e2b34b480674e5b1ebfe17
LouisTheLeaf <lherna1682@gmail.com>
2022-03-28 20:40:13 -0500

    merging stuff from dev

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template,request
 from SearchFunctions import *
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
+def getInfo():
+    data = searchDB()
+    return render_template('frontproto.html', data=data)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('frontproto.html')
+    name = request.form['name']
+    return render_template('frontproto.html', name=name)
