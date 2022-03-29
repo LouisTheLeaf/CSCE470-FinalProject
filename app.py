bb8f5f8749d368ab68eeee32ef16b15f44f08cf3
skye Elliott <SKyetheguy125@tamu.edu>
2022-03-28 20:25:42 -0500

    search string fixed

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template,request
 from SearchFunctions import *
+import psycopg2
+import psycopg2.extras
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
