2f2d41f44dfb144c79d883bf67d2f4acbbc60f6d
LouisTheLeaf <lherna1682@gmail.com>
2022-04-24 22:00:22 -0500

    Adding in final touches to the css and adding in error checking for user searches.

--- app.py
+++ app.py
@@ -4,29 +4,20 @@ import psycopg2
 import psycopg2.extras
 app = Flask(__name__, template_folder='templates')
 
-def getData():
-    print("ran getData()")
-    if request.method == "POST":
-        query_from_form = request.form['query']
-        print("getData query: "+query_from_form)
-        dbResults = searchDB( str(query_from_form) )
-    return dbResults
-
-#@app.route('/results', methods=['GET', 'POST'])
-#def results():
-#    return render_template('frontproto.html', data=data)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     profs = []
     rmp = []
     dbResults = [profs,rmp]
+    user_searched = 0
     if request.method == "POST":
+        user_searched = 1
         query_from_form = request.form['query']
         print("getData query: "+query_from_form)
-        if query_from_form:
+        if query_from_form.isdigit():
+            print("the query is not a string")
             dbResults = searchDB( str(query_from_form) )
-    return render_template('frontproto.html', data=dbResults)
+    return render_template('frontproto.html', data=dbResults, search=user_searched)
 
 if __name__ == '__main__':
     my_awesome_app.run()
